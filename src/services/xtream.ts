25e58a461b2b680802f30b7fe57b14856350c846
anisossss <anis.khalef@polytechnicien.tn>
2025-03-06 19:32:55 +0100

    first commit

--- src/services/xtream.ts
+++ src/services/xtream.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 class XtreamAPI {
-  proxy = "http://proxy.iptv-aicko.com";
+  proxy = "https://proxy.iptv-aicko.com";
   private readonly host: string;
   private readonly username: string;
   private readonly password: string;
